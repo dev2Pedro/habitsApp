a28499ed0fe7e635e5739358cdbd2503b605b5a6
pedroServfaz <pedro.servfaz@gmail.com>
2025-09-15 15:15:25 -0300

    Correção nas datas e dias do summaryTable com a lib dayjs

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -3,10 +3,7 @@ import * as Checkbox from "@radix-ui/react-checkbox";
 import { ProgressBarIndicator } from "./ProgressBar";
 import clsx from "clsx";
 import { Check } from "phosphor-react";
-import "dayjs/locale/pt-br";
 import dayjs from "dayjs";
-
-dayjs.locale("pt-br");
 
 interface HabitDayProps {
   date: Date;
@@ -18,8 +15,8 @@ export function HabitDay({ date, amount = 0, completed = 0 }: HabitDayProps) {
   const completedPercentage =
     amount > 0 ? Math.round((completed / amount) * 100) : 0;
 
-  const dayNumber = dayjs(date).format("DD/MM"); // ex: 27/08
-  const weekDay = dayjs(date).format("dddd"); // ex: segunda-feira
+  const dayAndMonth = dayjs(date).format("DD/MM");
+  const dayOfWeek = dayjs(date).format("dddd");
   return (
     <Popover.Root>
       <Popover.Trigger
@@ -39,9 +36,9 @@ export function HabitDay({ date, amount = 0, completed = 0 }: HabitDayProps) {
 
       <Popover.PopoverPortal>
         <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
-          <span className="font-semibold text-zinc-400">{weekDay}</span>
+          <span className="font-semibold text-zinc-400">{dayAndMonth}</span>
           <span className="mt-1 font-extrabold leading-tight text-3xl">
-            {dayNumber}
+            {dayOfWeek}
           </span>
 
           <ProgressBarIndicator progress={completedPercentage} />
